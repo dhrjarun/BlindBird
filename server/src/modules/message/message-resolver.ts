11f4f56b61972435517f2666923683a8e75f32fd
Dhiraj Arun <dhrjarun@gmail.com>
2022-08-31 17:38:44 +0530

    fix(server): fix the authorization issue of message resolvers

--- server/src/modules/message/message-resolver.ts
+++ server/src/modules/message/message-resolver.ts
@@ -15,7 +15,7 @@ import Message, { Sender } from '../../entity/Message'
 import Chat from '../../entity/Chat'
 import dataSource from '../../data-source'
 import User from '../../entity/User'
-import { AuthenticationError } from 'apollo-server-core'
+import { AuthenticationError, ForbiddenError } from 'apollo-server-core'
 
 export enum CursorType {
   BEFORE = 'before',
@@ -31,12 +31,27 @@ export class MessageResolver {
   @Authorized()
   @Query((returns) => [Message], { nullable: true })
   async messages(
+    @Ctx() context: MyCtx,
     @Arg('chatId') chatId: number,
     @Arg('cursor', { nullable: true }) cursor: number,
     @Arg('cursorType', (type) => CursorType, { nullable: true })
     cursorType: CursorType,
     @Arg('limit', { nullable: true }) limit: number,
   ): Promise<Message[]> {
+    const isAuthorized =
+      (await dataSource
+        .createQueryBuilder(Chat, 'chat')
+        .leftJoinAndSelect('chat.firstPerson', 'firstPerson')
+        .leftJoinAndSelect('chat.secondPerson', 'secondPerson')
+        .where('chat.id = :chatId', { chatId })
+        .andWhere('(firstPerson.id = :userId OR secondPerson.id = :userId)', {
+          userId: context.req.user?.id,
+        })
+        .getCount()) !== 0
+
+    if (!isAuthorized)
+      throw new ForbiddenError("You don'n have access to this message")
+
     const cursorString =
       cursorType === CursorType.AFTER ? 'message.id > :id' : 'message.id < :id'
     limit = limit ? limit : 12
@@ -59,6 +74,26 @@ export class MessageResolver {
   }
 
   @Authorized()
+  @Query(() => Message, { nullable: true })
+  async message(@Arg('id', () => Number) id: number, @Ctx() context: MyCtx) {
+    const msg = await dataSource
+      .createQueryBuilder(Message, 'message')
+      .leftJoin('message.chat', 'chat')
+      .leftJoin('chat.firstPerson', 'firstPerson')
+      .leftJoin('chat.secondPerson', 'secondPerson')
+      .where(
+        '(firstPerson.id = :userId OR secondPerson.id = :userId) AND message.id = :messageId',
+        {
+          messageId: id,
+          userId: context.req.user?.id,
+        },
+      )
+      .getOne()
+
+    return msg
+  }
+
+  @Authorized()
   @Mutation((returns) => Message, { nullable: true })
   async createMessage(
     @Ctx() context: MyCtx,
@@ -74,9 +109,12 @@ export class MessageResolver {
       .leftJoinAndSelect('chat.secondPerson', 'secondPerson')
       .select(['chat', 'firstPerson.id', 'secondPerson.id'])
       .where('chat.id = :chatId', { chatId })
+      .andWhere('(firstPerson.id = :userId OR secondPerson.id = :userId)', {
+        userId: context.req.user?.id,
+      })
       .getOne()
 
-    if (!chat) return Promise.reject('No chat with this id in the database')
+    if (!chat) throw new ForbiddenError("You don't have access to this message")
 
     const sender = new User()
     sender.id = context.req.user!.id
@@ -117,13 +155,30 @@ export class MessageResolver {
   @Authorized()
   @Mutation((returns) => Boolean)
   async markSeen(@Arg('messageId') messageId: number, @Ctx() context: MyCtx) {
-    const result = await dataSource
+    const message = await dataSource
       .createQueryBuilder(Message, 'message')
-      .update(Message)
-      .set({ isSeen: true })
-      .where('message.id = :messageId', { messageId })
-      .execute()
-    if (result.affected === 1) return true
-    return false
+      .leftJoin('message.chat', 'chat')
+      .leftJoin('chat.firstPerson', 'firstPerson')
+      .leftJoin('chat.secondPerson', 'secondPerson')
+      .where(
+        '((firstPerson.id = :userId AND message.sender = :sP) OR (secondPerson.id = :userId AND message.sender = :fP)) AND message.id = :messageId',
+        {
+          messageId,
+          userId: context.req.user?.id,
+          fP: Sender.FIRST_PERSON,
+          sP: Sender.SECOND_PERSON,
+        },
+      )
+      .getOne()
+
+    if (!message) return false
+
+    if (!message.isSeen) {
+      message.isSeen = true
+      await message.save()
+      return true
+    }
+
+    return true
   }
 }
